02dadb434eeabf448135130d2d0610ee4eda8497
Faiz Mohammad <email2faizm@gmail.com>
2026-07-16 11:02:35 +0530

    Add files via upload

---
+++ Workplace Information Security Awareness/story_content/user.js
@@ -0,0 +1,30 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  // Force the window or tab to close cleanly
+window.close();
+
+// Fallback for LMS environments (scans for parent windows)
+if (typeof(PlayerConfiguration) !== 'undefined') {
+    window.parent.close();
+}
+}
+
+};
